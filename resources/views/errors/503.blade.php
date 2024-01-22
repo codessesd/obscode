db605695cf191b841a0322e05c494a01bfc5c021
Rendz <rendani@obcode.co.za>
2024-01-22 13:31:42 +0200

    =Vendor published Laravel errors

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,5 @@
+@extends('errors::minimal')
+
+@section('title', __('Website under maintenance.'))
+@section('code', '503')
+@section('message', __('Website under scheduled maintenance. Please check back later.'))
