f2ea5e83fcb240afbabc248948b94b50d778fcad
Rendz <rendani@obcode.co.za>
2024-01-22 15:05:02 +0200

    =Edited Maitenance message.

--- resources/views/errors/503.blade.php
+++ resources/views/errors/503.blade.php
@@ -2,4 +2,4 @@
 
 @section('title', __('Website under maintenance.'))
 @section('code', '503')
-@section('message', __('Website under scheduled maintenance. Please check back later.'))
+@section('message', __('We are under maintenance. Be back soon!'))
